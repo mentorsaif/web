415d14c30bc538a5c124d454245842f191badc92
Syed Saif ur Rahman <mentorsaif@hotmail.com>
2018-07-27 10:33:20 +1000

    Adding Asp.Net MVC Project

---
+++ MyAspMvc/MyAspMvc/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyAspMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+		// GET: Home
+		//public ActionResult Index()
+		//{
+		//    return View();
+		//}
+
+		//public string Index()
+		//{
+		//	return "Hello World";
+		//}
+
+		public ViewResult Index()
+		{
+			return View();
+		}
+	}
+}
