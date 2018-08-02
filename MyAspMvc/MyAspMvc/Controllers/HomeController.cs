f4ba95c04f6c200c807895b3f6e508964ca436bb
Syed Saif ur Rahman <mentorsaif@hotmail.com>
2018-08-02 14:50:22 +1000

    Added first validations

--- MyAspMvc/MyAspMvc/Controllers/HomeController.cs
+++ MyAspMvc/MyAspMvc/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyAspMvc.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -25,9 +26,23 @@ namespace MyAspMvc.Controllers
 			return View();
 		}
 
+		[HttpGet]
 		public ViewResult RsvpForm()
 		{
 			return View();
+		}
+
+		[HttpPost]
+		public ViewResult RsvpForm(GuestResponse guestResponse)
+		{
+			if (ModelState.IsValid)
+			{
+				return View("Thanks", guestResponse);
+			}
+			else
+			{
+				return View();
+			}
 		}
 	}
 }
--- MyAspMvc/MyAspMvc/Models/GuestResponse.cs
+++ MyAspMvc/MyAspMvc/Models/GuestResponse.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace MyAspMvc.Models
 {
 	public class GuestResponse
 	{
+		[Required(ErrorMessage = "Please enter your name")]
 		public string Name { get; set; }
+
+		[Required(ErrorMessage = "Please enter your email address")]
+		[RegularExpression(".+\\@.+\\..", ErrorMessage ="Please enter a valid email address")]
 		public string Email { get; set; }
+
+		[Required(ErrorMessage = "Please enter your phone number")]
 		public string Phone { get; set; }
+
+		[Required(ErrorMessage = "Please specify whether you'll attent")]
 		public bool? WillAttent { get; set; }
 	}
 }
