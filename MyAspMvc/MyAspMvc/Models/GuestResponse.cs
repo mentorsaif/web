8f0bfdf3278fc80ec3e78a644cf70d0a79da04b7
Syed Saif ur Rahman <mentorsaif@hotmail.com>
2018-08-06 13:12:19 +1000

    Completed my first MVC sample application Thanks to Adam Freeman for writing Pro ASP.NET MVC 5

--- MyAspMvc/MyAspMvc/Models/GuestResponse.cs
+++ MyAspMvc/MyAspMvc/Models/GuestResponse.cs
@@ -12,7 +12,7 @@ namespace MyAspMvc.Models
 		public string Name { get; set; }
 
 		[Required(ErrorMessage = "Please enter your email address")]
-		[RegularExpression(".+\\@.+\\..", ErrorMessage ="Please enter a valid email address")]
+		[RegularExpression(@"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$", ErrorMessage ="Please enter a valid email address")]
 		public string Email { get; set; }
 
 		[Required(ErrorMessage = "Please enter your phone number")]
