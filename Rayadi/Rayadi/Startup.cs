3af8f23ce46865952125d106659dbb34c66f4e73
Syed Saif ur Rahman <mentorsaif@hotmail.com>
2018-08-31 16:00:02 +1000

    Adding MVC core

--- Rayadi/Rayadi/Startup.cs
+++ Rayadi/Rayadi/Startup.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace Rayadi
 {
 	public class Startup
 	{
+
+		//public Startup(IConfigurationRoot configuration)
+		//{
+		//	Configuration = configuration;
+		//}
+
+		//public IConfigurationRoot Configuration { get;  }
+
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
@@ -35,12 +46,37 @@ namespace Rayadi
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
+				//app.UseBrowserLink();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
 			}
 
-			app.Run(async (context) =>
+			//No secure
+			app.UseStaticFiles();
+
+			//app.UseStaticFiles(new StaticFileOptions()
+			//{
+			//	FileProvider = new PhysicalFileProvider(
+			//		Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\MyArchive")),
+			//	RequestPath = new PathString("/Archive")
+			//}
+			//	);
+
+			//app.Run(async (context) =>
+			//{
+			//	await context.Response.WriteAsync("Hello World!");
+			//});
+
+			app.UseMvc(routes =>
 			{
-				await context.Response.WriteAsync("Hello World!");
-			});
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{Action=Index}/{id?}"
+					);
+			}
+				);
 		}
 	}
 }
